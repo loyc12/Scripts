635c386eded8287cfd1187f0ca67de4a447c794e
Loyc <lordloyc@gmail.com>
2023-12-01 13:59:17 -0500

    minor

--- .gitquick.sh
+++ .gitquick.sh
@@ -1,21 +1,21 @@
 # gq alias script
 
-if [[: $(git status) -eq 0 ]]
+if [ $(git status) -eq 0 ]
 then
 	echo "\n$RED Cannot use git here !!!$DEFCOL \n"
-elif [[ $(git status | grep -c "main") -gt 0 ]]
+elif [ $(git status | grep -c "main") -gt 0 ]
 then
 	echo "\n$RED Cannot use gitquick on branch main !!!$DEFCOL \n"
-elif [[ $(git status | grep -c "MAIN") -gt 0 ]]
+elif [ $(git status | grep -c "MAIN") -gt 0 ]
 then
 	echo "\n$RED Cannot use gitquick on branch MAIN !!!$DEFCOL \n"
-elif [[ $(git status | grep -c "master") -gt 0 ]]
+elif [ $(git status | grep -c "master") -gt 0 ]
 then
 	echo "\n$RED Cannot use gitquick on branch master !!!$DEFCOL \n"
-elif [[ $(git status | grep -c "MASTER") -gt 0 ]]
+elif [ $(git status | grep -c "MASTER") -gt 0 ]
 then
 	echo "\n$RED Cannot use gitquick on branch MASTER !!!$DEFCOL \n"
-elif [[ $(git status | grep -c "STABLE") -gt 0 ]]
+elif [ $(git status | grep -c "STABLE") -gt 0 ]
 then
 	echo "\n$RED Cannot use gitquick on branch STABLE !!!$DEFCOL \n"
 else
