19c69db373d62669ade189fe1d152413b3560858
Loyc12 <lordloyc@gmail.com>
2024-03-22 13:21:52 -0400

    minor

--- .load.sh
+++ .load.sh
@@ -4,3 +4,5 @@ export SCRIPTS=$HOME/Scripts
 source $SCRIPTS/.colours.sh
 source $SCRIPTS/.gitcmds.sh
 source $SCRIPTS/.help.sh
+
+# load the scripts by runing 'source $SCRIPTS/.load.sh' at the end of .zshrc or equivalent
