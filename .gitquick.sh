c75d636f8c9b671020027ae6096a07c13b45d3dc
Loyc12 <lordloyc@gmail.com>
2024-04-29 15:57:02 -0400

    switched from echo to printf

--- .gitquick.sh
+++ .gitquick.sh
@@ -3,10 +3,10 @@
 if git rev-parse --is-inside-work-tree > /dev/null 2>&1
 then
 	c_branch=$(git branch | grep \* | cut -d ' ' -f2)
-	echo "$MAGENTA You are about to autopush these files to the current branch ( $c_branch ) :$DEFCOL \n"
-	git status -s ; echo ""
+	printf "$MAGENTA You are about to autopush these files to the current branch ( $c_branch ) :$DEFCOL \n\n"
+	git status -s ; printf "\n"
 else
-	echo "\n$RED Cannot use git here !!!$DEFCOL \n"
+	printf "\n$RED Cannot use git here !!!$DEFCOL \n\n"
 	exit 1
 fi
 
@@ -14,7 +14,7 @@ $SCRIPTS/.yes.sh
 
 if [ $? -eq 1 ]
 then
-	echo "\n$RED Exiting the script !$DEFCOL \n"
+	printf "\n$RED Exiting the script !$DEFCOL \n\n"
 	exit 1
 else
 		git remote update || true
@@ -24,6 +24,6 @@ else
 		git status -s || true
 		git commit -m 'minor (gitquick)' || true
 		git push || true
-		echo "\n$BLUE > ! Done ! <$DEFCOL \n"
+		printf "\n$BLUE > ! Done ! <$DEFCOL \n\n"
 fi
 
--- .yes.sh
+++ .yes.sh
@@ -2,14 +2,14 @@
 
 # asks the user if they want to continue with the script
 
-echo "$YELLOW Are you sure you want to continue ? ( y | yes )$DEFCOL \n"
+printf "$YELLOW Are you sure you want to continue ? ( y | yes )$DEFCOL \n\n"
 read answer
 
 case $answer in
   [yY]|[yY][eE][sS])
-    echo "\n$BLUE Continuing ...$DEFCOL \n"
+    printf "\n$BLUE Continuing ...$DEFCOL \n\n"
 	exit 0 ;;
   *)
-    echo "\n$RED Exiting !$DEFCOL \n"
+    printf "\n$RED Exiting !$DEFCOL \n\n"
 	exit 1 ;;
 esac
