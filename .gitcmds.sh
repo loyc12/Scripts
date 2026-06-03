1b4c731e881d4abe75a9a16a1e6012e8777f7bbb
loyc12 <lordloyc@gmail.com>
2026-06-03 14:43:18 -0400

    Revert "minor tweaks to git diff"
    This reverts commit 2d6d67ff5ef8a6c8ba5f42e7774189aa7ab3c8c3.

--- .gitcmds.sh
+++ .gitcmds.sh
@@ -12,13 +12,13 @@ alias GU='printf "\n$GREEN > updating remote repos $DEFCOL \n\n" ; git remote up
 alias GA='printf "\n$GREEN > adding all changes to commit queue$MAGENTA !!!$DEFCOL \n\n" ; git add -A ; git add -u' # git add -A & -u
 alias GC='printf "\n$GREEN > commiting change queue with given message$DEFCOL \n\n" ; git commit -m'                # git commit < message >
 alias GB='printf "\n$GREEN > listing all branches$DEFCOL \n\n" ; git branch -a ; printf "\n"'                       # git list (branch -a)
-alias GD='printf "\n$GREEN > comparing given branches$DEFCOL \n\n" ; git diff --stat'                               # git diff < branch_1 > < branch_2 >
+alias GD='printf "\n$GREEN > comparing given branches$DEFCOL \n\n" ; git diff'                                      # git diff < branch_1..branch_1 >
 alias GG='printf "\n$GREEN > switching to given branch$DEFCOL \n\n" ; git switch'                                   # git go (switch)
 
 alias GFB='printf "\n$YELLOW > ! fetching from given branch !$DEFCOL \n\n" ; git fetch origin'                      # git fetch origin/< branch >
 alias GNB='printf "\n$YELLOW > ! pushing to new branch !$DEFCOL \n\n" ; git push origin'                            # git new_branch (push origin < src_branch:new_branch >)
+alias GDB='printf "\n$YELLOW > ! unfollowing given branch$DEFCOL \n\n" ; git branch -d'                             # git delete_branch (branch -d)
 alias GRA='printf "\n$MAGENTA > !! aborting rebase !!$DEFCOL \n\n" ; git rebase --abort'                            # git rebase --abort
-alias GRMB='printf "\n$YELLOW > ! unfollowing given branch$DEFCOL \n\n" ; git branch -d'                            # git remove_branch (branch -d)
 alias GMRG='printf "\n$MAGENTA > !! merging from given branch !!$DEFCOL \n\n" ; git merge'                          # git merge < src_branch >
 alias GPSH='printf "\n$MAGENTA > !! pushing commited changes to branch !!$DEFCOL \n\n" ; git push ; printf "\n"'    # git push
 alias GQCKP='printf "\n$RED > !!! making quick commit !!!$DEFCOL \n\n" ; $SCRIPTS/.gitquick.sh'                     # git quickpush
@@ -30,7 +30,7 @@ alias ZR='printf "\n$GREEN > ! calling zig build !$DEFCOL \n\n" ; zig build run'
 alias ZLC='printf "\n$GREEN > ! counting the lines of .zig files !$DEFCOL \n\n" ; find . -name "*.zig" | xargs wc -l' # find all named *.zig AND wc -l
 
 # MISC COMMANDS
-alias SRLD='printf "\n$GREEN > ! reloading all scripts !$DEFCOL \n\n" ; source $SCRIPTS/.cmds.sh'
+alias SRLD='printf "\n$GREEN > ! reloading all scripts !$DEFCOL \n\n" ; source $SCRIPTS/.gitcmds.sh'
 alias PKGU='printf "\n$GREEN > ! updating all packages !$DEFCOL \n\n" ; $SCRIPTS/.pkg-update.sh'
 alias SNUP='printf "\n$GREEN > ! updating the snap store !$DEFCOL \n\n" ; sudo snap refresh snap-store'
 alias TEST='printf "\n$RED T$MAGENTA E$YELLOW S$GREEN T$BLUE E$CYAN D$DEFCOL \n\n"'
